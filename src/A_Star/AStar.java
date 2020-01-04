f7d780a238c23a06fa940f13d2576b3abdf86ffd
abod11alh <50719223+abod11alh@users.noreply.github.com>
2020-01-04 16:36:39 +0200

    Finish get Correct Theater

--- src/A_Star/AStar.java
+++ src/A_Star/AStar.java
@@ -1,12 +1,15 @@
 package A_Star;
 
-import Models.*;
+import Models.PeriodInformation;
+import Models.Rule;
+import Models.StateTableClass;
+import Models.TheaterInformation;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AStar {
 
@@ -18,96 +21,66 @@ public class AStar {
     public AStar(Rule rule) {
         this.rule = rule;
         stateTables = new ArrayList<>();
-        rule.getTheaters().sort(new Comparator<TheaterInformation>() {
-            @Override
-            public int compare(final TheaterInformation o1, final TheaterInformation o2) {
-                // TODO: implement your logic here
-                if (o1.getStudents_size() >= o2.getStudents_size()) {
-                    return 1;
-                }
-
-                return -1;
-
+        rule.getTheaters().sort((o1, o2) -> {
+            // TODO: implement your logic here
+            if (o1.getStudents_size() >= o2.getStudents_size()) {
+                return 1;
             }
-        });
 
+            return -1;
+
+        });
+//        List<Integer> t = getTheaterSize();
+//        List<Integer> temp_list = t.stream().collect(Collectors.toList());
+//        getBestTheaterBasedStudentNumber(200, temp_list);
+//        System.out.println();
 
     }
 
-    public List<Integer> getTheaterSize()
-    {
-        List<Integer> theaterSize=new ArrayList<>();
-        List<TheaterInformation> theaterInformations=rule.getTheaters();
-        for (int i = 0; i <theaterInformations.size() ; i++) {
-            theaterSize.add(theaterInformations.get(i).getStudents_size());
+
+    public void AStare() {
+        List<Integer> theaters_size = getTheaterSize();
+        for (int day = 0; day < this.rule.getDays_information().size(); day++) {
+            List<PeriodInformation> periods = this.rule.getDays_information().get(day).getPeriod_information();
+            for (PeriodInformation period1 : periods) {
+                List<Integer> temp_theaters_size=new ArrayList<>();
+                Collections.copy(theaters_size,temp_theaters_size);
+                List<Integer> thater_indexs = getBestTheaterBasedStudentNumber(period1.getTotal_student(),temp_theaters_size);
+
+            }
+        }
+
+    }
+
+
+    private List<Integer> getTheaterSize() {
+        List<Integer> theaterSize = new ArrayList<>();
+        List<TheaterInformation> theaterInformations = rule.getTheaters();
+        for (TheaterInformation theaterInformation : theaterInformations) {
+            theaterSize.add(theaterInformation.getStudents_size());
         }
         return theaterSize;
     }
 
-    public void AStare() {
-        List<Integer> theaters_size=getTheaterSize();
-        for (int day = 0; day < this.rule.getDays_information().size(); day++) {
-            List<PeriodInformation> periods = this.rule.getDays_information().get(day).getPeriod_information();
-            for (int period = 0; period < periods.size(); period++) {
-                List<Integer> thater_indexs = getBestTheaterBasedStudentNumber(periods.get(period).getTotal_student(),theaters_size);
-
-            }
-        }
-
-    }
-
-    private List<Integer> getBestTheaterBasedStudentNumber(int total_student,List<Integer> theaters_size) {
-        BinarySearch binarySearch=new BinarySearch();
-        List<Integer> best_theaters=new ArrayList<>();
-        while (total_student>0)
-        {
-            Pair<Integer,Integer> pair=binarySearch.binarySearch(theaters_size,0,theaters_size.size(),total_student);
-            total_student-=pair.getValue();
+    private List<Integer> getBestTheaterBasedStudentNumber(int total_student, List<Integer> theaters_size) {
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> best_theaters = new ArrayList<>();
+        while (total_student > 0) {
+            Pair<Integer, Integer> pair = binarySearch.binarySearch(theaters_size, 0, theaters_size.size()-1, total_student);
+            total_student -= pair.getValue();
             best_theaters.add(pair.getKey());
+            theaters_size.remove(pair.getValue());
 
         }
+        System.out.println(best_theaters.size());
 
-        return  best_theaters;
+        return best_theaters;
     }
 
 
-    public boolean isFinal(StateTableClass stateTable) {
+    private boolean IsFinal(StateTableClass stateTable) {
 
         return true;
     }
-
-    public StateTableClass getNewState() {
-        //Operation
-
-
-        return null;
-    }
-
-
-    // getter and setter
-
-    public Rule getRule() {
-        return rule;
-    }
-
-    public void setRule(Rule rule) {
-        this.rule = rule;
-    }
-
-    public List<StateTableClass> getStateTables() {
-        return stateTables;
-    }
-
-    public void setStateTables(List<StateTableClass> stateTables) {
-        this.stateTables = stateTables;
-    }
-
-    public void addStates() {
-        StateTableClass stateTable = getNewState();
-        while (isFinal(stateTable)) {
-            stateTables.add(getNewState());
-        }
-    }
-
 
 }
